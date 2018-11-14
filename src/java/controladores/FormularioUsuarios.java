745c4c612f34ff8a91935ca0dce8c679833e2032
stefania <stefania1219@hotmail.com>
2018-11-14 10:39:37 -0500

    Cambios
    cambios en el formulario de usuario

--- src/java/controladores/FormularioUsuarios.java
+++ src/java/controladores/FormularioUsuarios.java
@@ -3,8 +3,8 @@
  * and open the template in the editor.
  */
 package controladores;
- import java.io.IOException;
-import java.io.PrintWriter;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,31 +14,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import modelos.Usuario;
- /**
+
+/**
  *
  * @author Usuario
  */
 public class FormularioUsuarios extends HttpServlet {
-     /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        private void guardarUsuario(String nombre, String correo, String contrasenia) {
+
+    private void guardarUsuario(String nombre, String correo, String contrasenia) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/hashbook_tag", "root", "");
@@ -54,8 +42,8 @@ public class FormularioUsuarios extends HttpServlet {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
- private List<Usuario> todosLosUsuario() throws SQLException {
+
+    private List<Usuario> todosLosUsuario() throws SQLException {
         Connection conexion = null;
         List<Usuario> Usuario = new ArrayList<Usuario>();
         try {
@@ -80,7 +68,7 @@ public class FormularioUsuarios extends HttpServlet {
         } catch (SQLException ex) {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-            if(conexion != null && !conexion.isClosed()) {
+            if (conexion != null && !conexion.isClosed()) {
                 conexion.close();
             }
         }
@@ -104,5 +92,4 @@ public class FormularioUsuarios extends HttpServlet {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-}
 }
