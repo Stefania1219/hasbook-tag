b83fa35411b5e182718c98f3e02e574823cbc88e
stefania <stefania1219@hotmail.com>
2018-11-14 10:53:41 -0500

    Eliminé
    Eliminé unos import que no necesitabamos

--- src/java/controladores/FormularioUsuarios.java
+++ src/java/controladores/FormularioUsuarios.java
@@ -4,7 +4,6 @@
  */
 package controladores;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,10 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import modelos.Usuario;
 
 /**
